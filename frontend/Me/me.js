d208b2fed2f91fcd4a8d1d62d85926a598990371
Max5201 <maxone520231@gmail.com>
2025-09-25 01:37:37 -0700

    Update me.js

--- frontend/Me/me.js
+++ frontend/Me/me.js
@@ -12,19 +12,3 @@ if (logoutBtn) {
     window.location.href = '/index.html'; // 根目录 index.html
   });
 }
-
-
-// 页面切换逻辑
-const buttons = document.querySelectorAll('.bottom-nav button');
-const pages = document.querySelectorAll('.page');
-
-buttons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const target = btn.getAttribute('data-page');
-    pages.forEach(p => p.classList.remove('active'));
-    buttons.forEach(b => b.classList.remove('active'));
-    const page = document.getElementById(target);
-    if(page) page.classList.add('active');
-    btn.classList.add('active');
-  });
-});
